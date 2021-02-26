6ae557ce04d68d4f64dd2a5a906cb75a273824c7
Earvin <earvin.tli@gmail.com>
2021-02-26 15:23:33 +0800

    feat: change to build a shared 'DataTable'

--- src/features/DriverDashboard/DriverTelemetryDataTable.js
+++ src/features/DriverDashboard/DriverTelemetryDataTable.js
@@ -2,65 +2,51 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-// Material-UI Dependencies
-import Table from '@material-ui/core/Table';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import Paper from '@material-ui/core/Paper';
+// Internal Dependencies
+import DataTable from '../../components/DataTable';
 
 const DriverTelemetryDataTable = () => {
   const {
     telemetryData: driverTelemetryData,
   } = useSelector((state) => state.driverDashboard);
 
-  const tableHeadCells = [
+  const tableHeadData = [
     'Origin',
     'Destination',
     'Speed',
     'Drive Duration',
     'Fuel Consumption',
     'Fuel Used',
-  ].map((headText) => <TableCell>{headText}</TableCell>);
+    'Telemetry Data Id',
+  ];
 
-  const tableBodyRows = driverTelemetryData.map((driverTelemetryDataToUse) => {
+  const tableBodyData = driverTelemetryData.map((driverTelemetryDataToUse) => {
     const {
-      id,
       origin,
       destination,
       speed,
       driveDuration,
       fuelConsumption,
       fuelUsed,
+      id,
     } = driverTelemetryDataToUse;
 
-    return (
-      <TableRow key={`${id}-${origin}-${destination}`}>
-        <TableCell>{origin}</TableCell>
-        <TableCell>{destination}</TableCell>
-        <TableCell>{speed}</TableCell>
-        <TableCell>{driveDuration}</TableCell>
-        <TableCell>{fuelConsumption}</TableCell>
-        <TableCell>{fuelUsed}</TableCell>
-      </TableRow>
-    );
+    return {
+      origin,
+      destination,
+      speed,
+      driveDuration,
+      fuelConsumption,
+      fuelUsed,
+      id,
+    };
   });
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            {tableHeadCells}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tableBodyRows}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <DataTable
+      tableHeadData={tableHeadData}
+      tableBodyData={tableBodyData}
+    />
   );
 };
 
